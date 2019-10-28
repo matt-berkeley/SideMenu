ed064a57cb8f6fc3314c4637537d2b27a9362664
Matt Ginelli <matt_uhs@me.com>
2019-10-27 16:58:24 -1000

    Initial Commit

---
+++ SideMenu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SideMenu
+//
+//  Created by Matthew Ginelli on 10/27/19.
+//  Copyright © 2019 MGinelli. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
